25d11ead12f89efc8a857156590f1225c0b7e403
IsmayilRahimov <rehimovismayil650@gmail.com>
2025-08-14 18:30:22 +0400

    security config//

---
+++ src/main/java/org/example/projectspring/ConfigSec.java
@@ -0,0 +1,23 @@
+package org.example.projectspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSec {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form.loginPage("/login"));
+        return http.build();
+
+
+    }
+}
